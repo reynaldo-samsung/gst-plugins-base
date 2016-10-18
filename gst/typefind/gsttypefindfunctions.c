3986f7b51815ecff2302500b10705b5f64cafc45
Reynaldo H. Verdejo Pinochet <reynaldo@osg.samsung.com>
2016-10-18 16:18:19 -0700

    typefind: add typefinder for iTunes itc artwork files
    Prevents:
    
    https://bugzilla.gnome.org/show_bug.cgi?id=773172

--- gst/typefind/gsttypefindfunctions.c
+++ gst/typefind/gsttypefindfunctions.c
@@ -422,6 +422,29 @@ uri_type_find (GstTypeFind * tf, gpointer unused)
   }
 }
 
+/*** application/itc ***/
+static GstStaticCaps itc_caps = GST_STATIC_CAPS ("application/itc");
+#define ITC_CAPS (gst_static_caps_get(&itc_caps))
+
+static void
+itc_type_find (GstTypeFind * tf, gpointer unused)
+{
+  DataScanCtx c = { 0, NULL, 0 };
+  guint8 magic[8] = { 0x00, 0x00, 0x01, 0x1C, 0x69, 0x74, 0x63, 0x68 };
+
+  GST_DEBUG ("ITC CHECK CALLED");
+  if (G_UNLIKELY (!data_scan_ctx_ensure_data (tf, &c, 8)))
+    return;
+
+  /* \0\0itch */
+  if (memcmp (c.data, magic, 8)) {
+    gst_type_find_suggest (tf, GST_TYPE_FIND_MAXIMUM, ITC_CAPS);
+    GST_DEBUG ("ITC MATCH");
+  }
+
+  return;
+}
+
 /*** application/x-hls ***/
 
 static GstStaticCaps hls_caps = GST_STATIC_CAPS ("application/x-hls");
@@ -5683,6 +5706,8 @@ plugin_init (GstPlugin * plugin)
       "txt", UTF32_CAPS, NULL, NULL);
   TYPE_FIND_REGISTER (plugin, "text/uri-list", GST_RANK_MARGINAL, uri_type_find,
       "ram", URI_CAPS, NULL, NULL);
+  TYPE_FIND_REGISTER (plugin, "application/itc", GST_RANK_SECONDARY,
+      itc_type_find, "itc", ITC_CAPS, NULL, NULL);
   TYPE_FIND_REGISTER (plugin, "application/x-hls", GST_RANK_MARGINAL,
       hls_type_find, "m3u8", HLS_CAPS, NULL, NULL);
   TYPE_FIND_REGISTER (plugin, "application/sdp", GST_RANK_SECONDARY,
